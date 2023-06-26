85f0c5035da275f1a622704403147f1207fbd480
Adam Ginzberg <125328015+adam-baserun@users.noreply.github.com>
2023-06-26 13:33:21 -0700

    Update API again (#10)

--- src/openai.ts
+++ src/openai.ts
@@ -3,32 +3,31 @@ import {
   CreateCompletionRequest,
   CreateChatCompletionRequest,
 } from 'openai';
-import { Provider } from './provider';
-
-interface Variables {
-  /* eslint-disable-next-line  @typescript-eslint/no-explicit-any */
-  variables?: string[];
-}
+import { BaserunProvider, BaserunType, Variables } from './types';
 
 export type OpenAIChatMessage = ChatCompletionRequestMessage & Variables;
 type OpenAICompletionPrompt = { content: string } & Variables;
 
-interface OpenAIChatRequestInput {
+export interface OpenAIChatRequestInput {
   config: Omit<CreateChatCompletionRequest, 'messages'>;
   messages: OpenAIChatMessage[];
-  provider: Provider.OpenAI;
+  provider: BaserunProvider.OpenAI;
+  type: BaserunType.Chat;
 }
 
-interface OpenAICompletionRequestInput {
+export interface OpenAICompletionRequestInput {
   config: Omit<CreateCompletionRequest, 'prompt'>;
   prompt: OpenAICompletionPrompt;
-  provider: Provider.OpenAI;
+  provider: BaserunProvider.OpenAI;
+  type: BaserunType.Completion;
 }
 
 export type OpenAIRequestInput =
   | OpenAIChatRequestInput
   | OpenAICompletionRequestInput;
 
-export type OpenAIRequest =
-  | CreateChatCompletionRequest
-  | CreateCompletionRequest;
+export type OpenAIChatRequestOutput = CreateChatCompletionRequest;
+export type OpenAICompletionRequestOutput = CreateCompletionRequest;
+export type OpenAIRequestOutput =
+  | OpenAIChatRequestOutput
+  | OpenAICompletionRequestOutput;

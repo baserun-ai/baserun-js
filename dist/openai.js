a3f4397f0227ab551d8ee1da90685f6ec1466967
Adam Ginzberg <adam@baserun.ai>
2023-07-28 10:24:09 -0700

    Delete old code

--- dist/openai.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.OpenAIChatRole = void 0;
-const openai_1 = require("openai");
-Object.defineProperty(exports, "OpenAIChatRole", { enumerable: true, get: function () { return openai_1.ChatCompletionRequestMessageRoleEnum; } });
--- dist/test/prompts.d.ts
+++
@@ -1,8 +0,0 @@
-import { GoogleChatRequestInput, GoogleCompletionRequestInput } from '../google';
-import { OpenAIChatRequestInput, OpenAICompletionRequestInput } from '../openai';
-import { LlamaChatRequestInput } from '../llama';
-export declare const OpenAICompletionPrompts: Record<string, OpenAICompletionRequestInput>;
-export declare const GoogleCompletionPrompts: Record<string, GoogleCompletionRequestInput>;
-export declare const OpenAIChatPrompts: Record<string, OpenAIChatRequestInput>;
-export declare const GoogleChatPrompts: Record<string, GoogleChatRequestInput>;
-export declare const LlamaChatPrompts: Record<string, LlamaChatRequestInput>;

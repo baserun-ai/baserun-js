390005dcd4dcedcc2fa78087db2c4074734b7f29
Adam Ginzberg <adam@baserun.ai>
2023-07-28 10:26:14 -0700

    Add initial cut of SDK

---
+++ src/tests/baserun-jest/jest_environment.test.ts
@@ -0,0 +1,41 @@
+import { Baserun } from '../../baserun';
+import * as baserun from '../../index';
+
+jest.mock('axios');
+
+/*
+ * BASERUN_API_KEY="test-key" npx jest src/tests/baserun-jest --config src/tests/baserun-jest/jest.config.js
+ */
+describe('BaserunJestEnvironment', () => {
+  let appendToBufferSpy: jest.SpyInstance;
+
+  beforeEach(() => {
+    appendToBufferSpy = jest.spyOn(Baserun, '_appendToBuffer');
+  });
+
+  afterEach(() => {
+    appendToBufferSpy.mockRestore();
+  });
+
+  it('test_explicit_log', () => {
+    baserun.log('TestEvent', 'whatever');
+
+    const bufferData = appendToBufferSpy.mock.calls[0][0];
+    expect(bufferData['name']).toBe('TestEvent');
+    expect(bufferData['payload']).toBe('whatever');
+  });
+
+  it('test_explicit_log_with_payload', () => {
+    const logName = 'TestEvent';
+    const logPayload = {
+      action: 'called_api',
+      value: 42,
+    };
+
+    baserun.log(logName, logPayload);
+
+    const bufferData = appendToBufferSpy.mock.calls[0][0];
+    expect(bufferData['name']).toBe(logName);
+    expect(bufferData['payload']).toEqual(logPayload);
+  });
+});

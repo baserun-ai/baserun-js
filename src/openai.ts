a8a8172df06553d31bd12176761b10b087b7afc4
Adam Ginzberg <adam@baserun.ai>
2023-08-06 11:13:53 -0500

    Update readme for multiple jest presets and support openai-edge

--- src/openai.ts
+++ src/openai.ts
@@ -1,11 +1,94 @@
 import { BaserunProvider, BaserunStepType, BaserunType, Log } from './types';
 import { getTimestamp } from './helpers';
 
+export function monkeyPatchOpenAIEdge(log: (entry: Log) => void) {
+  try {
+    /* eslint-disable-next-line @typescript-eslint/no-var-requires */
+    const openaiEdge = require('openai-edge');
+    const originalCompletion = openaiEdge.OpenAIApi.prototype.createCompletion;
+    const originalChatCompletion =
+      openaiEdge.OpenAIApi.prototype.createChatCompletion;
+
+    openaiEdge.OpenAIApi.prototype.createCompletion = async function (
+      /* eslint-disable-next-line  @typescript-eslint/no-explicit-any */
+      ...args: any[]
+    ): /* eslint-disable-next-line  @typescript-eslint/no-explicit-any */
+    Promise<any> {
+      const startTime = getTimestamp();
+      const response = await originalCompletion.bind(this)(...args);
+      const clonedResponse = response.clone();
+
+      const { prompt = '', ...config } = args[0] ?? {};
+      const data = await clonedResponse.json();
+      const output = data.choices[0].text;
+      const usage = data.usage;
+
+      const logEntry = {
+        stepType: BaserunStepType.AutoLLM,
+        type: BaserunType.Completion,
+        provider: BaserunProvider.OpenAI,
+        config,
+        prompt: { content: prompt },
+        output,
+        startTimestamp: startTime,
+        completionTimestamp: getTimestamp(),
+        usage,
+      };
+
+      log(logEntry);
+
+      return response;
+    };
+
+    openaiEdge.OpenAIApi.prototype.createChatCompletion = async function (
+      /* eslint-disable-next-line  @typescript-eslint/no-explicit-any */
+      ...args: any[]
+    ): /* eslint-disable-next-line  @typescript-eslint/no-explicit-any */
+    Promise<any> {
+      const startTime = getTimestamp();
+      const response = await originalChatCompletion.bind(this)(...args);
+      const clonedResponse = response.clone();
+
+      const { messages = [], ...config } = args[0] ?? {};
+      const data = await clonedResponse.json();
+      const output = data.choices[0].message.content;
+      const usage = data.usage;
+
+      const logEntry = {
+        stepType: BaserunStepType.AutoLLM,
+        type: BaserunType.Chat,
+        provider: BaserunProvider.OpenAI,
+        config,
+        messages,
+        output,
+        startTimestamp: startTime,
+        completionTimestamp: getTimestamp(),
+        usage,
+      };
+
+      log(logEntry);
+
+      return response;
+    };
+  } catch (error) {
+    /* openai-edge isn't used */
+    if (
+      error &&
+      typeof error === 'object' &&
+      'code' in error &&
+      error.code === 'MODULE_NOT_FOUND'
+    ) {
+      return;
+    }
+
+    throw error;
+  }
+}
+
 export function monkeyPatchOpenAI(log: (entry: Log) => void) {
   try {
     /* eslint-disable-next-line @typescript-eslint/no-var-requires */
     const openai = require('openai');
-
     const originalCompletion = openai.OpenAIApi.prototype.createCompletion;
     const originalChatCompletion =
       openai.OpenAIApi.prototype.createChatCompletion;

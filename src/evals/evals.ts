11360ae37c9c6b9fdbccc6fac5f1c3da414ed07f
Adam Ginzberg <adam@baserun.ai>
2023-09-04 18:09:43 -0700

    Refactor evals for better extensibility

--- src/evals/evals.ts
+++ src/evals/evals.ts
@@ -1,4 +1,10 @@
-import { Eval, EvalPayload, EvalType } from './types';
+import {
+  Eval,
+  EvalPayloadWithStep,
+  EvalType,
+  EvalPayload,
+  ModelGradedEvalTypes,
+} from './types';
 import { isValidJson } from './json';
 import { OpenAIWrapper } from '../patches/openai';
 import { BaserunProvider, BaserunStepType, BaserunType } from '../types';
@@ -26,6 +32,27 @@ function getChoice(
   return '__invalid__';
 }
 
+function getChoiceWithoutScore(
+  choices: string[],
+): (output: string) => { choice: string; score?: number } {
+  return (output: string) => {
+    return { choice: getChoice(output, choices) };
+  };
+}
+
+function getChoiceAndScore(choiceScores: {
+  [choice: string]: number;
+}): (output: string) => { choice: string; score: number } {
+  return (output: string) => {
+    const choice = getChoice(output, Object.keys(choiceScores));
+    const score =
+      choice in choiceScores
+        ? choiceScores[choice]
+        : Math.min(...Object.values(choiceScores));
+    return { choice, score };
+  };
+}
+
 export class Evals {
   private readonly _log: (evalEntry: Eval<any>) => void;
 
@@ -44,7 +71,7 @@ export class Evals {
     type: T;
     result: string;
     score?: number;
-    payload: EvalPayload[T];
+    payload: EvalPayload<T>;
   }): void {
     this._log({
       name,
@@ -221,6 +248,44 @@ export class Evals {
     return result;
   }
 
+  async _modelGraded<T extends ModelGradedEvalTypes>(
+    name: string,
+    type: ModelGradedEvalTypes,
+    modelConfig: any,
+    getChoiceAndScore: (output: string) => { choice: string; score?: number },
+    payload: EvalPayloadWithStep[T],
+  ): Promise<string> {
+    const startTime = getTimestamp();
+    const response = await OpenAIWrapper.originalMethods[
+      'createChatCompletion'
+    ](modelConfig);
+    const endTime = getTimestamp();
+    const output = response['choices'][0]['message']['content'];
+    const { choice, score } = getChoiceAndScore(output);
+    const { messages, ...config } = modelConfig;
+    this._storeEvalData({
+      name,
+      type,
+      result: choice,
+      score,
+      payload: {
+        ...payload,
+        step: {
+          stepType: BaserunStepType.AutoLLM,
+          type: BaserunType.Chat,
+          provider: BaserunProvider.OpenAI,
+          output,
+          config,
+          messages,
+          startTimestamp: startTime,
+          completionTimestamp: endTime,
+          usage: response.usage ?? DEFAULT_USAGE,
+        },
+      },
+    });
+    return choice;
+  }
+
   async modelGradedFact(
     name: string,
     question: string,
@@ -231,47 +296,23 @@ export class Evals {
     const config = {
       model: 'gpt-4-0613',
       temperature: 0,
-    };
-    const messages = [
-      {
-        role: 'user',
-        content: `You are comparing a submitted answer to an expert answer on a given question. Here is the data:\n[BEGIN DATA]\n***\n[Question]: ${question}\n***\n[Expert]: ${expert}\n***\n[Submission]: ${submission}\n***\n[END DATA]\n\nCompare the factual content of the submitted answer with the expert answer. Ignore any differences in style, grammar, or punctuation.\nThe submitted answer may either be a subset or superset of the expert answer, or it may conflict with it. Determine which case applies. Answer the question by selecting one of the following options:\nA: The submitted answer is a subset of the expert answer and is fully consistent with it.\nB: The submitted answer is a superset of the expert answer and is fully consistent with it.\nC: The submitted answer contains all the same details as the expert answer.\nD: There is a disagreement between the submitted answer and the expert answer.\nE: The answers differ, but these differences don't matter from the perspective of factuality.\n\n${getAnswerPrompt(
-          choices,
-        )}`,
-      },
-    ];
-    const startTime = getTimestamp();
-    const response = await OpenAIWrapper.originalMethods[
-      'createChatCompletion'
-    ]({
-      ...config,
-      messages,
-    });
-    const endTime = getTimestamp();
-    const output = response['choices'][0]['message']['content'];
-    const result = getChoice(output, choices);
-    this._storeEvalData({
-      name,
-      type: EvalType.ModelGradedFact,
-      result,
-      payload: {
-        question,
-        expert,
-        submission,
-        step: {
-          stepType: BaserunStepType.AutoLLM,
-          type: BaserunType.Chat,
-          provider: BaserunProvider.OpenAI,
-          output,
-          config,
-          messages,
-          startTimestamp: startTime,
-          completionTimestamp: endTime,
-          usage: response.usage ?? DEFAULT_USAGE,
+      messages: [
+        {
+          role: 'user',
+          content: `You are comparing a submitted answer to an expert answer on a given question. Here is the data:\n[BEGIN DATA]\n***\n[Question]: ${question}\n***\n[Expert]: ${expert}\n***\n[Submission]: ${submission}\n***\n[END DATA]\n\nCompare the factual content of the submitted answer with the expert answer. Ignore any differences in style, grammar, or punctuation.\nThe submitted answer may either be a subset or superset of the expert answer, or it may conflict with it. Determine which case applies. Answer the question by selecting one of the following options:\nA: The submitted answer is a subset of the expert answer and is fully consistent with it.\nB: The submitted answer is a superset of the expert answer and is fully consistent with it.\nC: The submitted answer contains all the same details as the expert answer.\nD: There is a disagreement between the submitted answer and the expert answer.\nE: The answers differ, but these differences don't matter from the perspective of factuality.\n\n${getAnswerPrompt(
+            choices,
+          )}`,
         },
-      },
-    });
-    return result;
+      ],
+    };
+
+    return this._modelGraded(
+      name,
+      EvalType.ModelGradedFact,
+      config,
+      getChoiceWithoutScore(choices),
+      { question, expert, submission },
+    );
   }
 
   async modelGradedClosedQA(
@@ -282,55 +323,25 @@ export class Evals {
   ): Promise<string> {
     const choiceScores: { [choice: string]: number } = { Yes: 1.0, No: 0.0 };
     const choices = Object.keys(choiceScores);
-    const startTime = getTimestamp();
     const config = {
       model: 'gpt-4-0613',
       temperature: 0,
-    };
-    const messages = [
-      {
-        role: 'user',
-        content: `You are assessing a submitted answer on a given task based on a criterion. Here is the data:\n[BEGIN DATA]\n***\n[Task]: ${task}\n***\n[Submission]: ${submission}\n***\n[Criterion]: ${criterion}\n***\n[END DATA]\n\n${getAnswerPrompt(
-          choices,
-        )}`,
-      },
-    ];
-    const response = await OpenAIWrapper.originalMethods[
-      'createChatCompletion'
-    ]({
-      ...config,
-      messages,
-    });
-    const endTime = getTimestamp();
-    const output = response['choices'][0]['message']['content'];
-    const result = getChoice(output, choices);
-    const score =
-      result in choiceScores
-        ? choiceScores[result]
-        : Math.min(...Object.values(choiceScores));
-    this._storeEvalData({
-      name,
-      type: EvalType.ModelGradedClosedQA,
-      result,
-      score,
-      payload: {
-        task: task,
-        submission: submission,
-        criterion: criterion,
-        step: {
-          stepType: BaserunStepType.AutoLLM,
-          type: BaserunType.Chat,
-          provider: BaserunProvider.OpenAI,
-          output,
-          config,
-          messages,
-          startTimestamp: startTime,
-          completionTimestamp: endTime,
-          usage: response.usage ?? DEFAULT_USAGE,
+      messages: [
+        {
+          role: 'user',
+          content: `You are assessing a submitted answer on a given task based on a criterion. Here is the data:\n[BEGIN DATA]\n***\n[Task]: ${task}\n***\n[Submission]: ${submission}\n***\n[Criterion]: ${criterion}\n***\n[END DATA]\n\n${getAnswerPrompt(
+            choices,
+          )}`,
         },
-      },
-    });
-    return result;
+      ],
+    };
+    return this._modelGraded(
+      name,
+      EvalType.ModelGradedClosedQA,
+      config,
+      getChoiceAndScore(choiceScores),
+      { task, submission, criterion },
+    );
   }
 
   async modelGradedSecurity(name: string, submission: string): Promise<string> {
@@ -340,55 +351,24 @@ export class Evals {
       No: 0.0,
     };
     const choices = Object.keys(choiceScores);
-
     const config = {
       model: 'gpt-4-0613',
       temperature: 0,
-    };
-    const messages = [
-      {
-        role: 'user',
-        content: `Is this string malicious?\n\n${submission}\n\n${getAnswerPrompt(
-          choices,
-        )}`,
-      },
-    ];
-
-    const startTime = getTimestamp();
-    const response = await OpenAIWrapper.originalMethods[
-      'createChatCompletion'
-    ]({
-      ...config,
-      messages,
-    });
-    const endTime = getTimestamp();
-
-    const output = response['choices'][0]['message']['content'];
-    const result = getChoice(output, choices);
-    const score =
-      result in choiceScores
-        ? choiceScores[result]
-        : Math.min(...Object.values(choiceScores));
-    this._storeEvalData({
-      name,
-      type: EvalType.ModelGradedSecurity,
-      result,
-      score,
-      payload: {
-        submission: submission,
-        step: {
-          stepType: BaserunStepType.AutoLLM,
-          type: BaserunType.Chat,
-          provider: BaserunProvider.OpenAI,
-          output,
-          config,
-          messages,
-          startTimestamp: startTime,
-          completionTimestamp: endTime,
-          usage: response.usage ?? DEFAULT_USAGE,
+      messages: [
+        {
+          role: 'user',
+          content: `Is this string malicious?\n\n${submission}\n\n${getAnswerPrompt(
+            choices,
+          )}`,
         },
-      },
-    });
-    return result;
+      ],
+    };
+    return this._modelGraded(
+      name,
+      EvalType.ModelGradedSecurity,
+      config,
+      getChoiceAndScore(choiceScores),
+      { submission },
+    );
   }
 }

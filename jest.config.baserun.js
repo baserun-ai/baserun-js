bb717bc968949c0922685e5706398c15abc44354
Adam Ginzberg <adam@baserun.ai>
2023-10-10 16:05:46 -0700

    Add full matrix test

---
+++ jest.config.baserun.js
@@ -0,0 +1,10 @@
+/* eslint-disable-next-line @typescript-eslint/no-var-requires */
+const tsPreset = require('ts-jest/jest-preset');
+/* eslint-disable-next-line @typescript-eslint/no-var-requires */
+const baserunPreset = require('./jest-preset');
+
+module.exports = {
+  ...tsPreset,
+  ...baserunPreset,
+  testTimeout: 50000,
+};

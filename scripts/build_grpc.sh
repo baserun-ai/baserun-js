5cb72271f728a2511ddeaea0d7ba4002e4fe5e54
Erik Peterson <e@eriklp.com>
2023-11-07 11:29:34 -0800

    Add missing build script (#74)

---
+++ scripts/build_grpc.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env /bin/bash
+grpc_tools_node_protoc \
+--plugin=protoc-gen-ts=./node_modules/.bin/protoc-gen-ts \
+--plugin=protoc-gen-grpc=./node_modules/.bin/grpc_tools_node_protoc_plugin \
+--proto_path=src/v1/protos \
+--js_out=import_style=commonjs,binary:src/v1/generated \
+--grpc_out=grpc_js:src/v1/generated \
+--ts_out=grpc_js:src/v1/generated \
+src/v1/protos/baserun.proto
+

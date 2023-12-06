4d1679e3522b1573faa66c70b7430d9b4f638f93
Simon Patole <patolesimon@gmail.com>
2023-12-07 03:09:44 +0800

    Sp ts annotation (#86)
    * Typescript Annotations + example
    
    * Fix annotation file format

--- src/index.ts
+++ src/index.ts
@@ -6,5 +6,6 @@ const log = Baserun.log;
 const evals = Baserun.evals;
 const session = Baserun.session;
 const flush = Baserun.flush;
+const annotate = Baserun.annotate;
 
-export const baserun = { init, log, trace, evals, session, flush };
+export const baserun = { init, log, trace, evals, session, flush, annotate };
